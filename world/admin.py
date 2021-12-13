cb567ea3cbdf324dadc229d5d822b79fb8d4406f
りんりん <max3max7@icloud.com>
2021-12-13 23:08:35 +0900

    PostGISへデータのインポートとモデルの定義、管理画面の設定の変更を行いました

--- world/admin.py
+++ world/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.gis import admin
+from world.models import Border, School, Facility, Busstop
+
+admin.site.register(Border, admin.GeoModelAdmin)
+admin.site.register(School, admin.GeoModelAdmin)
+admin.site.register(Facility, admin.GeoModelAdmin)
+admin.site.register(Busstop, admin.GeoModelAdmin)
